a35ae983d532f97929b1400af1c1f60ee0dd3bfd
Erica Riello <Ericariello@Ericas-MacBook-Pro-2.local>
2015-04-21 12:37:33 -0300

    Pacman desenhado em ponto passado
    Agora o Pacman pode ser desenhado em uma direção e em coordenadas
    definidas.

--- MiniJogo/Pacman-style/jogo.elm
+++ MiniJogo/Pacman-style/jogo.elm
@@ -1,7 +1,8 @@
 import Color
 import Graphics.Collage 
 import Graphics.Element
- 
+type Direction = Up | Down | Right | Left
+map: List (List Int)
 map = [[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
     [ -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1],
     [ -1,  1, -1, -1, -1, -1,  1, -1, -1, -1, -1, -1,  1, -1, -1,  1, -1, -1, -1, -1, -1,  1, -1, -1, -1, -1,  1, -1],
@@ -33,41 +34,52 @@ map = [[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     [ -1,  1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  1, -1, -1,  1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  1, -1],
     [ -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1],
     [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]]
+
+tileSize : number
+tileSize = 15
  
 getSquaresFromArray : List Int -> Float -> Float -> List Graphics.Collage.Form 
 getSquaresFromArray xs x y = 
     case xs of
         [] -> []
-        hd::tl -> (Graphics.Collage.move (x, y) (Graphics.Collage.filled (getBlockColor hd) (Graphics.Collage.rect 20 20))) :: (getCircle hd x y) ++ (getSquaresFromArray tl (x+20) y)
+        hd::tl -> (Graphics.Collage.move (x, y) (Graphics.Collage.filled (getBlockColor hd) (Graphics.Collage.rect tileSize tileSize))) :: (getCircle hd x y) ++ (getSquaresFromArray tl (x+tileSize) y)
 
 getSquaresFromMatrix : List (List Int) -> Float -> Float -> List Graphics.Collage.Form
 getSquaresFromMatrix xs x y = 
     case xs of
         [] -> []
-        hd::tl -> (getSquaresFromArray hd x y) ++ (getSquaresFromMatrix tl x (y-20))
+        hd::tl -> (getSquaresFromArray hd x y) ++ (getSquaresFromMatrix tl x (y-tileSize))
 
 getCircle : Int -> Float -> Float -> List Graphics.Collage.Form
 getCircle i x y =
     case i of
         -1 -> []
         0 -> []
-        1 -> (Graphics.Collage.move (x, y) (Graphics.Collage.filled white (Graphics.Collage.circle 2)))::[]
-        2 -> (Graphics.Collage.move (x, y) (Graphics.Collage.filled white (Graphics.Collage.circle 5)))::[]
+        1 -> (Graphics.Collage.move (x, y) (Graphics.Collage.filled white (Graphics.Collage.circle 1.5)))::[]
+        2 -> (Graphics.Collage.move (x, y) (Graphics.Collage.filled white (Graphics.Collage.circle 4)))::[]
         3 -> []
         4 -> []
 
-getPacmanForm : Graphics.Element.Element
-getPacmanForm =
-    Graphics.Element.image 15 15 "./images/pacman_right.png"
+getPacmanForm : Float -> Float -> Direction -> Graphics.Collage.Form
+getPacmanForm x y dir =
+    Graphics.Collage.move (x, y) (Graphics.Collage.toForm (Graphics.Element.image 15 15 (pacmanImagePath dir)))
 
+pacmanImagePath : Direction -> String
+pacmanImagePath dir =
+    case dir of
+        Up -> "./images/pacman_up.png"
+        Down -> "./images/pacman_down.png"
+        Left -> "./images/pacman_left.png"
+        Right -> "./images/pacman_right.png"
 
 getAllForms : List Graphics.Collage.Form
 getAllForms = 
-    (getSquaresFromMatrix map -270 300)
+    (getSquaresFromMatrix map (-27*tileSize/2) (30*tileSize/2))
  
 main : Graphics.Element.Element
 main =
-    Graphics.Element.flow Graphics.Element.outward ((Graphics.Collage.collage (28*20) (31*20) getAllForms) :: (getPacmanForm) ::[])
+    Graphics.Element.flow Graphics.Element.outward (
+        (Graphics.Collage.collage (28*tileSize) (31*tileSize) (getAllForms++((getPacmanForm 0 0 Right)::[])))::[])
  
 getBlockColor : Int -> Color.Color
 getBlockColor i =
